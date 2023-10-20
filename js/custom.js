faf5637cd6c1dedfbb4e1217a1b16b33412d9df3
BankThanawit <bankavbn2545@gmail.com>
2023-10-20 21:58:36 +0700

    update

--- js/custom.js
+++ js/custom.js
@@ -1,86 +1,32 @@
-var hero = $(".hero_area");
-var menu = $(".custom_menu-container");
-$(window).scroll(function () {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > hero.height()) {
-        menu.addClass("menu_fixed-position");
-        $(".custom_menu-container + section").addClass("mt-5");
-    } else {
-        menu.removeClass("menu_fixed-position");
-        $(".custom_menu-container + section").removeClass("mt-5");
-    }
-});
 
-
-var myCircle = Circles.create({
-    id: "circles-1",
-    radius: 65,
-    value: 85,
-    maxValue: 100,
-    width: 5,
-    text: function (value) {
-        return value + "%";
-    },
-    colors: ["transparent", "#ffffff"],
-    duration: 400,
-    wrpClass: "circles-wrp",
-    textClass: "progress_text",
-    valueStrokeClass: "circles-valueStroke",
-    maxValueStrokeClass: "circles-maxValueStroke",
-    styleWrapper: true,
-    styleText: true
-});
-var myCircle = Circles.create({
-    id: "circles-2",
-    radius: 65,
-    value: 55,
-    maxValue: 100,
-    width: 5,
-    text: function (value) {
-        return value + "%";
-    },
-    colors: ["transparent", "#ffffff"],
-    duration: 400,
-    wrpClass: "circles-wrp",
-    textClass: "progress_text",
-    valueStrokeClass: "circles-valueStroke",
-    maxValueStrokeClass: "circles-maxValueStroke",
-    styleWrapper: true,
-    styleText: true
-});
-var myCircle = Circles.create({
-    id: "circles-3",
-    radius: 65,
-    value: 65,
-    maxValue: 100,
-    width: 5,
-    text: function (value) {
-        return value + "%";
-    },
-    colors: ["transparent", "#ffffff"],
-    duration: 400,
-    wrpClass: "circles-wrp",
-    textClass: "progress_text",
-    valueStrokeClass: "circles-valueStroke",
-    maxValueStrokeClass: "circles-maxValueStroke",
-    styleWrapper: true,
-    styleText: true
-});
-var myCircle = Circles.create({
-    id: "circles-4",
-    radius: 65,
-    value: 85,
-    maxValue: 100,
-    width: 5,
-    text: function (value) {
-        return value + "%";
-    },
-    colors: ["transparent", "#ffffff"],
-    duration: 400,
-    wrpClass: "circles-wrp",
-    textClass: "progress_text",
-    valueStrokeClass: "circles-valueStroke",
-    maxValueStrokeClass: "circles-maxValueStroke",
-    styleWrapper: true,
-    styleText: true
-});
+(function ($) {
+  
+    "use strict";
+  
+      // PRE LOADER
+      $(window).load(function(){
+        $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
+      });
+  
+      // NAVBAR
+      $(".navbar").headroom();
+  
+      $('.navbar-collapse a').click(function(){
+          $(".navbar-collapse").collapse('hide');
+      });
+  
+      $('.slick-slideshow').slick({
+        autoplay: true,
+        infinite: true,
+        arrows: false,
+        fade: true,
+        dots: true,
+      });
+  
+      $('.slick-testimonial').slick({
+        arrows: false,
+        dots: true,
+      });
+      
+    })(window.jQuery);
+  
