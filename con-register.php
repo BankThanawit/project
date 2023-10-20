daee77226edcb012eb4c0464392fe1d01baa9ddf
Chaiwut <chivwut0646@gmail.com>
2023-10-20 16:03:03 +0700

    update

---
+++ con-register.php
@@ -0,0 +1,23 @@
+<?php
+    $user_id = $_POST['user_id'];
+    $user_name = $_POST['user_name'];
+    $user_fname = $_POST['user_fname'];
+    $user_lname = $_POST['user_lname'];
+    $address = $_POST['address'];
+    $pass = $_POST['pass'];
+    $phonenumber = $_POST['phonenumber'];
+
+    $conn = new mysqli('localhost','root','','mnp');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into user_(user_id, user_name, user_fname, user_lname, address, pass, phonenumber) values(?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssss", $user_id, $user_name, $user_fname, $user_lname, $address, $pass, $phonenumber);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
