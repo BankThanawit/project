d0f6c307b54083066884690a98af828c318f325d
BankThanawit <bankavbn2545@gmail.com>
2023-10-29 19:23:38 +0700

    แก้

--- add-product.php
+++ add-product.php
@@ -2,15 +2,15 @@
 $product_name = $_POST['product_name'];
 $product_price = $_POST['product_price'];
 $product_stype = $_POST['product_stype'];
-$product_image = $_FILES['imags']['name'];
+$imags = $_FILES['imags']['name'];
 $product_image_tmp_name = $_FILES['imags']['tmp_name'];
-$product_image_folder = 'images/' . $product_image;
+$product_image_folder = 'imags/' . $imags;
 
 $conn = new mysqli('localhost', 'root', '', 'mnp');
-if (empty($product_name) || empty($product_price) || empty($product_stype) || empty($product_image)) {
+if (empty($product_name) || empty($product_price) || empty($product_stype) || empty($imags)) {
    $message[] = 'please fill out all';
 } else {
-   $insert = "INSERT INTO product(product_name, product_price, product_stype, imags) VALUES('$product_name', '$product_price', '$product_stype', '$product_image')";
+   $insert = "INSERT INTO product(product_name, product_price, product_stype, imags) VALUES('$product_name', '$product_price', '$product_stype', '$imags')";
    $upload = mysqli_query($conn, $insert);
    if ($upload) {
       move_uploaded_file($product_image_tmp_name, $product_image_folder);
